42e60e785a730a9298e422d5f382bcc3941db558
aflores <itt.alejandro.flores@hotmail.com>
2022-05-11 22:34:34 -0500

    Primero

---
+++ lib/main.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondRoute()),
+              );
+            },
+            child: Image(
+              image: AssetImage('assets/13.png'),
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            )));
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MyApp2()),
+        );
+      },
+      child: Image(
+        image: AssetImage('assets/1.png'),
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+      ),
+    ));
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ThirdRoute(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class ThirdRoute extends StatefulWidget {
+  const ThirdRoute({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<ThirdRoute> createState() => _ThirdRouteState();
+}
+
+class _ThirdRouteState extends State<ThirdRoute> {
+  final String txs = "8145687876";
+
+  final myController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    int count = 1;
+
+    return Scaffold(
+        body: Stack(
+      children: [
+        Image(
+          image: AssetImage('assets/5.png'),
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ),
+        Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FourthRoute()),
+                );
+              },
+              child: Image(
+                image: AssetImage('assets/3.png'),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                  child: TextField(
+                style: TextStyle(fontSize: 40),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.visiblePassword,
+                autocorrect: false,
+                enableSuggestions: false,
+                controller: myController,
+                onChanged: (te) {
+                  myController.text = txs.substring(0, count);
+                  myController.selection = TextSelection.fromPosition(
+                      TextPosition(offset: myController.text.length));
+                  count++;
+                },
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                ),
+              )),
+            ),
+            GestureDetector(
+              onTap: () {
+                myController.text = txs.substring(0, count);
+                myController.selection = TextSelection.fromPosition(
+                    TextPosition(offset: myController.text.length));
+                count++;
+              },
+              child: Image(
+                image: AssetImage('assets/4.png'),
+              ),
+            )
+          ],
+        )
+      ],
+    ));
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class FourthRoute extends StatelessWidget {
+  const FourthRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const FifthRoute()),
+        );
+      },
+      child: Image(
+        image: AssetImage('assets/2.png'),
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+      ),
+    ));
+  }
+}
+
+class FifthRoute extends StatelessWidget {
+  const FifthRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const SixtRoute()),
+        );
+      },
+      child: Image(
+        image: AssetImage('assets/13.png'),
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+      ),
+    ));
+  }
+}
+
+class SixtRoute extends StatelessWidget {
+  const SixtRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GestureDetector(
+      child: Image(
+        image: AssetImage('assets/6.png'),
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+      ),
+    ));
+  }
+}
